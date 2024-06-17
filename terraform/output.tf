2d12d50d955b39134b0dbb9f1e98c6395841624d
Oleh Kovalievskyi <fop.kovalievskyi@gmail.com>
2024-06-17 17:52:15 +0300

    output correct

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,3 @@
 output storage_name {
     value = module.storage.storage_name
 }
-
-output resource_group {
-    value = "name: ${azurerm_resource_group.current.name}, location: ${azurerm_resource_group.current.location}"
-}
